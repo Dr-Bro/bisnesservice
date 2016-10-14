d013ae71fcdd31f3703d30cf1a9d704349ac2f25
Dr-Bro <imb95@yandex.ru>
2016-10-15 00:01:50 +0300

    21
    big changes

--- js/callback.js
+++ js/callback.js
@@ -78,6 +78,7 @@ else if($("[name=buhsys]")[2].checked==true){
 
 
 $('#price').text(temp + ' Р');
+$('input[name=price]').text(temp);
 }
 
 
@@ -85,6 +86,7 @@ $('#price').text(temp + ' Р');
 $(document).ready(function(){
         callback();
         callback2();
+        callback3();
         //Скрыть PopUp при загрузке страницы 
 
         PopUpHide();
@@ -129,7 +131,7 @@ url: m_action,
 data: m_data,
 success: function() {  //сообщение об удачной отправке
                         jQuery('#send-form').html("<div id='message'></div>");  
-                        jQuery('#message').html("<h2>Вопрос отправлен!</h2>")  
+                        jQuery('#message').html("<h2>Сообщение отправлено!</h2>")  
                         .append("<p>Скоро мы с вами свяжемся.</p>")  
                         .hide()  
                         .fadeIn(1500, function() {  
@@ -157,7 +159,36 @@ url: m_action,
 data: m_data,
 success: function() {  //сообщение об удачной отправке
                         jQuery('#send-form2').html("<div id='message'></div>");  
-                        jQuery('#message').html("<h2>Заявка отправлена!</h2>")  
+                        jQuery('#message').html("<h2>Сообщение отправлено!</h2>")  
+                        .append("<p>Скоро мы с вами свяжемся.</p>")  
+                        .hide()  
+                        .fadeIn(1500, function() {  
+                        $('#message').append("<i class=\"icon-ok\"></i>");  
+                        });  
+                }
+});
+});
+};
+
+
+ function callback3(){
+$('#send-form3').submit(function(e){
+//отменяем стандартное действие при отправке формы
+e.preventDefault();
+//берем из формы метод передачи данных
+var m_method=$(this).attr('method');
+//получаем адрес скрипта на сервере, куда нужно отправить форму
+var m_action=$(this).attr('action');
+//получаем данные, введенные пользователем в формате input1=value1&input2=value2...,
+//то есть в стандартном формате передачи данных формы
+var m_data=$(this).serialize();
+$.ajax({
+type: m_method,
+url: m_action,
+data: m_data,
+success: function() {  //сообщение об удачной отправке
+                        jQuery('#send-form2').html("<div id='message'></div>");  
+                        jQuery('#message').html("<h2>Сообщение отправлено!</h2>")  
                         .append("<p>Скоро мы с вами свяжемся.</p>")  
                         .hide()  
                         .fadeIn(1500, function() {  
